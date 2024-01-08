f9ae7b4b1fc0ffb6f6a9d0c84839b5776f14371c
IamLikhon1 <likhonadnan55@gmail.com>
2024-01-08 15:03:46 +0600

    add service page data show

--- src/Pages/ServicesPage/Service.jsx
+++ src/Pages/ServicesPage/Service.jsx
@@ -1,10 +1,44 @@
+import { useQuery } from "@tanstack/react-query"
 import SubBanner from "../../SharedComponents/SubBanner/SubBanner"
+import Loader from "../../SharedComponents/Loader/Loader"
+import BookCard from "../../SharedComponents/BookCard/BookCard"
 
 function Service() {
+  const { isLoading, data } = useQuery({
+    queryKey: ['servicesData'],
+    queryFn: () =>
+      fetch('http://localhost:5000/getAllServiceData').then((res) =>
+        res.json()
+      ),
+  })
+  if (isLoading) return <Loader />
   return (
     <div>
       {/* sub banner */}
-      <SubBanner title="Service" subTitle="Services Page"/>
+      <SubBanner title="Service" subTitle="Services Page" />
+      <div className="max-w-7xl mx-auto">
+        <div className="lg:flex items-center justify-between">
+          {/* search */}
+          <div className="flex mt-5">
+            <input type="text" className="mt-5 ml-2 lg:ml-0 w-fit px-8 lg:px-44 py-4 rounded-lg border-2 focus:outline-none bg-[#F3F3F3]" placeholder="Search by Service Name" />
+            <button className='px-4 lg:px-14  rounded-md border-2 border-[#F7A582] text-[#F7A582] font-semibold mt-4 lg:mt-5 ml-3 hover:bg-[#F7A582] hover:text-white duration-500'>Search</button>
+          </div>
+          {/* sort */}
+          <div>
+            {/* <button className="relative left-16 lg:left-0 px-10 py-4 rounded-md border-2 border-[#07332F] text-[#07332F] font-semibold mt-7 ml-3 hover:bg-[#07332F] hover:text-white duration-500">
+           
+            </button> */}
+          </div>
+
+        </div>
+        <div className="grid lg:grid-cols-3 gap-5 lg:gap-8 mt-10 mx-2 lg:mx-0">
+          {
+            data?.map(item => (
+              <BookCard key={item._id} item={item} />
+            ))
+          }
+        </div>
+      </div>
     </div>
   )
 }
--- src/SharedComponents/BookCard/BookModal/BookModal.jsx
+++ src/SharedComponents/BookCard/BookModal/BookModal.jsx
@@ -1,55 +1,77 @@
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 
 const BookModal = () => {
     const {
         register,
         handleSubmit,
         reset,
-      } = useForm()
+    } = useForm()
 
-      const onSubmit = (data) => {
+    const onSubmit = (data) => {
         const name = data.name
+        const service_name = data.service
         const number = data.number
         const email = data.email;
-        const allInfo={name,number,email}
+        const allInfo = { name, service_name, number, email }
+
+        fetch('http://localhost:5000/postUserServiceData', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(allInfo)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    toast.success('Your Appointment set successfully')
+                }
+            })
         reset()
-        console.log(allInfo)
-    
-      };
+
+    };
     return (
         <dialog id="my_modal_2" className="modal">
-                <div className="modal-box text-center">
-                    <form method="dialog">
-                        {/* if there is a button in form, it will close the modal */}
-                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-                    </form>
-                    <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="modal-box text-center">
+                <form method="dialog">
+                    {/* if there is a button in form, it will close the modal */}
+                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                </form>
+                <form onSubmit={handleSubmit(onSubmit)}>
 
-              {/* name */}
-             
-              <input {...register("name", { required: true })} type="text" name="name" className="mt-5 lg:ml-1 mb-5 w-full px-5 py-3 rounded-md border-2 focus:outline-none bg-[#F3F3F3]" placeholder="Full Name"
-              />
+                    {/* name */}
 
-              <br />
+                    <input {...register("name", { required: true })} type="text" name="name" className="mt-5 lg:ml-1 mb-5 w-full px-5 py-3 rounded-md border-2 focus:outline-none bg-[#F3F3F3]" placeholder="Full Name"
+                    />
 
-              {/* Number */}
-              
-              <input {...register("number", { required: true })} type="text" name="number" className="mt-2 lg:ml-1 mb-5 w-full px-5 py-3 rounded-md border-2 focus:outline-none bg-[#F3F3F3]" placeholder="Phone Number"
-              />
+                    <br />
 
-              <br />
+                    {/* serviceName */}
 
-              {/* Email */}
-             
-              <input {...register("email", { required: true })} type="email" name="email" className=" mt-2 lg:ml-1 w-full  px-5 py-3 rounded-md border-2 focus:outline-none bg-[#F3F3F3]" placeholder="Email" />
+                    <input {...register("service", { required: true })} type="text" name="service" className="mt-2 lg:ml-1 mb-5 w-full px-5 py-3 rounded-md border-2 focus:outline-none bg-[#F3F3F3]" placeholder="Service Name"
+                    />
 
-              {/* button */}
-              <div>
-                <input type="submit" value='Book' className="mt-8 w-full  px-5 py-3 rounded-md border-2 bg-[#07332F] text-white cursor-pointer font-medium" />
-              </div>
-            </form>
-                </div>
-            </dialog>
+                    <br />
+
+                    {/* Number */}
+
+                    <input {...register("number", { required: true })} type="text" name="number" className="mt-2 lg:ml-1 mb-5 w-full px-5 py-3 rounded-md border-2 focus:outline-none bg-[#F3F3F3]" placeholder="Phone Number"
+                    />
+
+                    <br />
+
+                    {/* Email */}
+
+                    <input {...register("email", { required: true })} type="email" name="email" className=" mt-2 lg:ml-1 w-full  px-5 py-3 rounded-md border-2 focus:outline-none bg-[#F3F3F3]" placeholder="Email" />
+
+                    {/* button */}
+                    <div>
+                        <input type="submit" value='Book' className="mt-8 w-full  px-5 py-3 rounded-md border-2 bg-[#07332F] text-white cursor-pointer font-medium" />
+                    </div>
+                </form>
+            </div>
+        </dialog>
 
     );
 };
