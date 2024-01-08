94ef664b79ab2b938fec8bed56db927a0b29f82b
IamLikhon1 <likhonadnan55@gmail.com>
2024-01-08 12:54:34 +0600

    add get api for appoinment page

--- src/Pages/AppoinmentPage/AppointmentPage.jsx
+++ src/Pages/AppoinmentPage/AppointmentPage.jsx
@@ -1,10 +1,37 @@
+import { useQuery } from "@tanstack/react-query"
 import SubBanner from "../../SharedComponents/SubBanner/SubBanner"
+import Loader from "../../SharedComponents/Loader/Loader"
+
+import appointmentBanner from '../../assets/AppBanner.png'
 
 function AppointmentPage() {
+  const { isLoading, data } = useQuery({
+    queryKey: ['servicesData'],
+    queryFn: () =>
+      fetch('http://localhost:5000/getAllServiceData').then((res) =>
+        res.json()
+      ),
+  })
+  if (isLoading) return <Loader />
+
+  const appointmentData = data.filter(item => item.category == 'Appointment');
+  console.log(appointmentData);
   return (
     <div>
       {/* Sub banner */}
-      <SubBanner title="Appointment" subTitle="Appointment Page"/>
+      <SubBanner title="Appointment" subTitle="Appointment Page" />
+
+      <div className="max-w-7xl mx-auto ">
+        <div className="grid lg:grid-cols-2 gap-5 mt-8 mx-2">
+          <div>
+            555
+          </div>
+          <div className="">
+            <img src={appointmentBanner} alt="" />
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
