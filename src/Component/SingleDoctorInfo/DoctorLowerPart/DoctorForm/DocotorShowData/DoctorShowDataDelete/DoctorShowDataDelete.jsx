61b5dd1723bb19ead5d9719ae89c3d56d53d65d1
IamLikhon1 <likhonadnan55@gmail.com>
2024-01-08 11:28:09 +0600

    add delete logic on modal

--- src/Component/SingleDoctorInfo/DoctorLowerPart/DoctorForm/DocotorShowData/DoctorShowDataDelete/DoctorShowDataDelete.jsx
+++ src/Component/SingleDoctorInfo/DoctorLowerPart/DoctorForm/DocotorShowData/DoctorShowDataDelete/DoctorShowDataDelete.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import toast from "react-hot-toast"
 
 function DoctorShowDataDelete({ item, refetch }) {
+    const [inputUser, setInputUser] = useState('');
+
     const { _id } = item
     const handleDelete = (id) => {
         fetch(`http://localhost:5000/deleteReview/${id}`, {
@@ -14,20 +17,29 @@ function DoctorShowDataDelete({ item, refetch }) {
                 refetch()
             })
 
-    }
+    };
+
+    const handleInputChange = (event) => {
+        const value = event.target.value;
+        setInputUser(value);
+    };
     return (
         <>
-             <dialog id="my_modal_1" className="modal">
-                    <div className="modal-box text-center">
-                        <form method="dialog">
-                            {/* if there is a button in form, it will close the modal */}
-                            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-                        </form>
-                        <h2 className="text-xl mt-2 text-[#3B3A3A] font-semibold">Do you want to Delete this Review</h2>
-                        <button onClick={() => handleDelete(_id)} className="mt-5  px-10 py-4 rounded-lg border-2 bg-[#F7A582] text-white cursor-pointer" >Delete This Review</button>
-                    </div>
-                </dialog>
-            
+            <dialog id="my_modal_1" className="modal">
+                <div className="modal-box text-center">
+                    <form method="dialog">
+                        {/* if there is a button in form, it will close the modal */}
+                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                    </form>
+                    <h2 className="text-base mt-2 text-[#3B3A3A] ">If you want to Delete this Review please Type below :-  <span className="text-[#F7A582] font-semibold">Delete</span></h2>
+                    {/* input */}
+                    <input onChange={handleInputChange} type="text" name="text" className="mt-4 lg:ml-1 mb-4  px-5 py-3 rounded-md border-2 focus:outline-none bg-[#F3F3F3] lg:w-[80%]" placeholder="Type 'Delete' word"
+                    />
+
+                    <button  disabled={inputUser !== 'Delete'}  onClick={() => handleDelete(_id)} className="px-10 py-4 rounded-lg border-2 bg-[#F7A582] text-white cursor-pointer" >Delete This Review</button>
+                </div>
+            </dialog>
+
         </>
     )
 }
