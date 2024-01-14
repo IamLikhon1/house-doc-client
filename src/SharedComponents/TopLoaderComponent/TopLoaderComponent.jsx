c17d39aeca555aa1872944935304a03592419d80
IamLikhon1 <likhonadnan55@gmail.com>
2024-01-14 12:12:04 +0600

    finish the project

--- src/SharedComponents/TopLoaderComponent/TopLoaderComponent.jsx
+++ src/SharedComponents/TopLoaderComponent/TopLoaderComponent.jsx
@@ -1,15 +1,15 @@
 import LoadingBar from "react-top-loading-bar";
 
-const TopLoaderComponent = ({progress,setProgress}) => {
+const TopLoaderComponent = ({ progress, setProgress }) => {
     return (
         <LoadingBar
-        color='#F7A582'
-        progress={progress}
-        height={5}
-        loaderSpeed={1000}
-        waitingTime={1500}
-        onLoaderFinished={() => setProgress(0)}
-      />
+            color='#F7A582'
+            progress={progress}
+            height={5}
+            loaderSpeed={1000}
+            waitingTime={1500}
+            onLoaderFinished={() => setProgress(0)}
+        />
     );
 };
 
